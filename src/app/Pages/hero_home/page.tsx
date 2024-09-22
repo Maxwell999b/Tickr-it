7c940ba90f3bb39765a34ff41d501f3b8b64cfb2
Abdulrahman Ahmed <bedo150@gmail.com>
2024-09-22 15:07:58 +0300

    update page.tsx

--- src/app/Pages/hero_home/page.tsx
+++ src/app/Pages/hero_home/page.tsx
@@ -6,7 +6,7 @@ export default function Hero_HomePage() {
       <section className="bg-background py-12 md:py-20">
         <div className="container">
           <div className="text-center space-y-4">
-            <h1 className="text-3xl font-bold md:text-4xl">Welcome to your task list</h1>
+            <h1 className="text-3xl font-bold text-pink-600 md:text-4xl">Welcome to your task list</h1>
             <p className="text-muted-foreground md:text-xl">Get started by adding a new task</p>
             <Add_New_Task />
           </div>
