ffc6afa914f8341e2784ee01e400768bb440d550
Abdulrahman Ahmed <bedo150@gmail.com>
2024-09-24 00:46:58 +0300

    update settings.json and tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,6 +75,7 @@ const config: Config = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+        base: "0.5rem",
       },
     },
   },
