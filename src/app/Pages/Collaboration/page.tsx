18923072df2f127a4c7ff004856c5f8f182743de
Abdulrahman Ahmed <bedo150@gmail.com>
2024-09-23 16:01:20 +0300

    update page.tsx

--- src/app/Pages/Collaboration/page.tsx
+++ src/app/Pages/Collaboration/page.tsx
@@ -13,7 +13,6 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
